1731885e10af2e0ee0afb6b074a33a5bef6cefe0
Corvette31 <corvette.ss@yandex.ru>
2022-07-12 00:58:58 +0300

    Добавьте файлы проекта.

---
+++ OOPTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOPTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHealth = 100;
+            float playerSpeed = 10f;
+            Player player = new Player("Alex", playerHealth, playerSpeed);
+
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private float _speed;
+
+        public Player(string name, int health, float speed)
+        {
+            _name = name;
+            _health = health;
+            _speed = speed;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Игрок - {_name} , Здоровье - {_health} , Скорость {_speed}");
+        }
+    }
+}
